c6ade0212f4be099dd34507033b3db9e9c430fc1
italo <italoiveldoluiz@gmail.com>
2024-11-13 08:44:52 -0300

    refactor: added documentation for healthController

--- auth/src/health/health.controller.ts
+++ auth/src/health/health.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class HealthController {
   @Get()
-  checkHealth() {
+  @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'Application is working successfully',
+    schema: {
+      type: 'object',
+      properties: {
+        status: { type: 'boolean' },
+      },
+    },
+  })
+  checkHealth(): Record<string, boolean> {
     return {
       status: true,
     };
